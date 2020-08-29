8cd2a5d6e78f9d017a6618feb38abbcb38c6a763
fuuuu <sanchan.fuu@gmail.com>
2020-08-29 14:44:48 +0900

    second commit

---
+++ backend/resources/views/Rd_content/index.blade.php
@@ -0,0 +1,94 @@
+@extends('app')
+
+@section('title', 'TOPページ')
+
+@section('content')
+ @include('nav')
+
+<div class="top-page">
+    <main>
+        <div class="row">
+            <div class="col">
+                <div></div>
+            </div>
+        </div>
+        <div class="row" style="margin: auto -15px">
+            <div
+            class="col-md-6 col-lg-7 offset-lg-0"
+            style="
+            margin: 0 auto;
+            margin-right: auto;
+            margin-left: auto;
+            width: 100%;
+          "
+        >
+        <div class="d-lg-flex justify-content-lg-center container"></div>
+        <p
+        class="top-text"
+        style="
+              color: rgb(223, 232, 238);
+              font-size: 25px;
+              margin-top: 30%;
+              margin-left: 5%;
+              line-height: 40px;
+            "
+          >
+          あなたは自身の被ばく線量を把握していますか？<br />毎月の被ばく線量を把握して、日々の業務を安全に従事するために<br>当サイトをご活用ください
+        </p>
+        <div
+        class="d-flex d-md-flex d-lg-flex justify-content-md-center align-items-lg-center top-link"
+        style="
+              width: 100%;
+              margin-top: 70px;
+              margin-right: auto;
+              margin-left: auto;
+              padding-right: auto;
+              padding-left: auto;
+            "
+          >
+          <a
+          class="btn btn-light"
+          role="button"
+          style="
+                margin-right: auto;
+                margin-left: auto;
+                color: #ffffff;
+                background-color: #6caee0;
+              "
+              href="login.html"
+              >ログイン</a
+              >
+            </div>
+            <div
+            class="d-flex d-md-flex d-lg-flex justify-content-md-center justify-content-lg-center top-link"
+            style="
+              margin-top: 21px;
+              margin-right: auto;
+              margin-left: auto;
+              width: 100%;
+            "
+          >
+          <a
+          class="btn btn-light d-lg-flex justify-content-lg-center top-btn"
+          role="button"
+          style="
+                margin-right: auto;
+                margin-left: auto;
+                text-align: center;
+                background-color: #6caee0;
+                color: #ffffff;
+              "
+              href="register.html"
+              >登録</a
+              >
+            </div>
+        </div>
+        <div class="col">
+            <div></div>
+        </div>
+    </div>
+    </main>
+
+</div>
+
+@endsection
---
+++ backend/resources/views/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <title>
+        @yield('title')
+    </title>
+
+    {{-- stylesheetのソースを指定 同一階層のassetsディレクトリに格納 --}}
+    <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Application-Form.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Article-List.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/dh-card-image-left-dark.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Login-Form-Clean.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Navigation-Clean.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Pretty-Registration-Form.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
+  </head>
+
+  <body>
+
+    @yield('content')
+
+    {{-- jQury --}}
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+    <script src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/Application-Form-1.js')}}"></script>
+    <script src="{{asset('assets/js/Application-Form.js')}}"></script>
+  </body>
+
+</html>
