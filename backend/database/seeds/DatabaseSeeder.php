a85ff2a0e433fd77476aa1f52f76b87d7ae15e19
fuuuu <sanchan.fuu@gmail.com>
2020-12-03 18:48:43 +0900

    dbalのキャッシュ利用

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             JobsTableSeeder::class,
             PositionsTableSeeder::class,
-            // ExposuresTableSeeder::class,
+            ExposuresTableSeeder::class,
         ]);
     }
 }
